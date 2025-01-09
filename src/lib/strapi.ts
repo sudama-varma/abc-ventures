78fa2966da66eb0792ebfa45553cfccba0554a6a
sudama-varma <sudama140varma30@gmail.com>
2025-01-09 12:26:14 +0530

    resolve

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -1,75 +1,74 @@
 import { fetchApi } from "@lib/api";
-import type { Post, PostData } from "../interfaces/post";
+import type { Blog, BlogData } from "../interfaces/blog";
 import type { Site, SiteData } from "../interfaces/site";
 import type { CategriesData } from "../interfaces/category";
 
-const nativePostsData: PostData[] = await fetchApi<PostData[]>({
-  endpoint: "posts",
+const nativeBlogsData: BlogData[] = await fetchApi<BlogData[]>({
+  endpoint: "blogs",
   query: {
-    "filters[site][name]": "Meddiah",
+    "filters[site][name]": "ABC Venture",
     sort: "updatedAt:desc",
     populate: "*",
   },
   wrappedByKey: "data",
 });
-
+console.log(nativeBlogsData)
 const siteData: SiteData[] = await fetchApi<SiteData[]>({
   endpoint: "sites",
   query: {
-    "filters[name]": "Meddiah",
+    "filters[name]": "ABC Venture",
     populate: "*",
   },
   wrappedByKey: "data",
 });
 
-export const nativePosts: Post[] = nativePostsData.map((post) => {
+export const nativeBlogs: Blog[] = nativeBlogsData.map((blog) => {
   return {
-    id: post.id,
-    title: post.attributes.title,
-    slug: post.attributes.slug,
-    content: post.attributes.content,
-    categories: post.attributes.categories.data.map(
-      (category) => category?.attributes?.name || "",
+    id: blog.id,
+    title: blog.title,
+    slug: blog.slug,
+    content: blog.content,
+    categories: blog.categories.map(
+      (category) => category?.name || "",
     ),
-    imageUrl: post.attributes.image.data?.attributes?.url || null,
-    imageWidth: post.attributes.image.data?.attributes?.width || null,
-    imageHeight: post.attributes.image.data?.attributes?.height || null,
+    imageUrl: blog.image?.url || null,
+    imageWidth: blog.image?.width || null,
+    imageHeight: blog.image?.height || null,
     imageSmallUrl:
-      post.attributes.image.data?.attributes?.formats?.small?.url || null,
+      blog.image?.formats?.small?.url || null,
     imageSmallWidth:
-      post.attributes.image.data?.attributes?.formats?.small?.width || null,
+      blog.image?.formats?.small?.width || null,
     imageSmallHeight:
-      post.attributes.image.data?.attributes?.formats?.small?.height || null,
+      blog.image?.formats?.small?.height || null,
     imageMediumUrl:
-      post.attributes.image.data?.attributes?.formats?.medium?.url || null,
+      blog.image?.formats?.medium?.url || null,
     imageMediumWidth:
-      post.attributes.image.data?.attributes?.formats?.medium?.width || null,
+      blog.image?.formats?.medium?.width || null,
     imageMediumHeight:
-      post.attributes.image.data?.attributes?.formats?.medium?.height || null,
-    createdAt: post.attributes.createdAt,
-    updatedAt: post.attributes.updatedAt,
-    publishedAt: post.attributes.publishedAt,
+      blog.image?.formats?.medium?.height || null,
+    createdAt: blog.createdAt,
+    updatedAt: blog.updatedAt,
+    publishedAt: blog.publishedAt,
   };
 });
 
 export const site: Site[] = siteData.map((site) => {
   return {
     id: site.id,
-    name: site.attributes.name,
-    slug: site.attributes.slug,
-    termsOfAgreement: site.attributes.termsOfAgreement,
-    privacyPolicy: site.attributes.privacyPolicy,
-    logoUrl: site.attributes.logo.data.attributes.url,
-    logoWidth: site.attributes.logo.data.attributes.width,
-    logoHeight: site.attributes.logo.data.attributes.height,
+    name: site.name,
+    slug: site.slug,
+    privacyPolicy: site.privacyPolicy,
+    logoUrl: site.logo?.url,
+    logoWidth: site.logo?.width,
+    logoHeight: site.logo?.height,
     logoSmallUrl:
-      site.attributes.logo.data.attributes.formats?.small?.url || null,
+      site.logo?.formats?.small?.url || null,
     logoSmallWidth:
-      site.attributes.logo.data.attributes.formats?.small?.width || null,
+      site.logo?.formats?.small?.width || null,
     logoSmallHeight:
-      site.attributes.logo.data.attributes.formats?.small?.height || null,
-    createdAt: site.attributes.createdAt,
-    updatedAt: site.attributes.updatedAt,
+      site.logo?.formats?.small?.height || null,
+    createdAt: site.createdAt,
+    updatedAt: site.updatedAt,
   };
 });
 
@@ -83,7 +82,7 @@ export const categories: CategriesData[] = (
   })
 ).filter(
   (cat) =>
-    !!cat.attributes.sites.data.filter(
-      (site) => site.attributes.name === "Meddiah",
+    !!cat.sites?.filter(
+      (site) => site.name === "ABC Venture",
     ).length,
 );
