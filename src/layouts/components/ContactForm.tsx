b00740b0bb818a576e02d2130a4f7368b564e07f
sudama-varma <sudama140varma30@gmail.com>
2025-01-10 13:15:13 +0530

    add contact page

---
+++ src/layouts/components/ContactForm.tsx
@@ -0,0 +1,45 @@
+
+import React from 'react'
+
+const ContactForm = () => {
+  return (
+    <div className='w-full contact-form'>
+        <form>
+            <label className='text-[#000] font-bold'>Your Name <span className='text-[#FF0000]'>*</span></label>
+            <div className='mb-3'>
+              <input type="text" className='p-2 rounded w-full' placeholder='First name' name="name" required />
+            </div>
+            <div className='mb-3'>
+              <input type="text" className='p-2 rounded w-full' placeholder='Last name' name="name" required />
+            </div>
+
+            <label className='text-[#000] font-bold'>Email <span className='text-[#FF0000]'>*</span></label>
+            <div className='mb-3'>
+              <input type="email" className='p-2 rounded w-full' placeholder='name@example.com' name="name" required />
+            </div>
+
+            <label className='text-[#000] font-bold'>Phone number</label>
+            <div className='mb-3'>
+              <input type="email" className='p-2 rounded w-full' placeholder='name@example.com' name="name" required />
+            </div>
+
+            <label className='text-[#000] font-bold'>Your Message</label>
+            <div className='mb-3'>
+              <textarea  className='p-2 rounded w-full' rows={5} placeholder='' name="name" required />
+            </div>
+            
+            <div>
+                <input type='checkbox' className="mr-2" />
+                <label>By submitting this form, you confirm that you have read and agree to ABC Venture’s 
+                     <a href='/privacy-policy/' className='underline ml-2'>Privacy Policy</a>
+                </label>
+            </div>
+            <div className='mt-2'>
+                <button className='btn btn-send-message'>Send Message</button>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default ContactForm
