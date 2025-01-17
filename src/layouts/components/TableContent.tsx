87dc316057e93919cf49321fa523ec514921616a
Geektech <development.geektech@gmail.com>
2025-01-17 17:38:52 +0530

    fix header

--- src/layouts/components/TableContent.tsx
+++ src/layouts/components/TableContent.tsx
@@ -17,10 +17,19 @@ const TableContent = ({ headings, customClass }: Headings) => {
       <ul className={customClass}>
         {
           headings?.map((heading: Heading, index: number) =>
-            <li key={index} onClick={()=>scrollToElement(heading?.id)} className='cursor-pointer'>
-              {heading?.heading}
-              {heading?.childs && <TableContent customClass={"ml-3 children"+index} headings={heading.childs} />}
-            </li>
+            <>
+              
+              <li key={index} onClick={()=>scrollToElement(heading?.id)} className='cursor-pointer'>
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                </svg>
+              </span>
+
+                {heading?.heading}
+                {heading?.childs && <TableContent customClass={"ml-3 table-children children"+index} headings={heading.childs} />}
+              </li>
+            </>
           )
         }
       </ul>
